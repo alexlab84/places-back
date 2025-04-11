f003022ab26ccbf9c1e416cb64113b0b8bde59c9
ale <alexlab84@hotmail.com>
2025-04-11 15:13:16 +0200

    Add changes in serializers

--- places_app/serializers.py
+++ places_app/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Place
 
 class PlacesSerializer(serializers.ModelSerializer):
+    category_display = serializers.CharField(source='get_category_display', read_only=True)
+
     class Meta:
         model = Place
-        fields = '__all__'
+        fields = ['id', 'name', 'location', 'category', 'category_display', 'description']
